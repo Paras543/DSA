396db7126a70ac6d5b1acd23dbb078e877749f93
Paras543 <parasgupta0561@gmail.com>
2026-07-27 11:52:02 +0530

    Done

---
+++ Arrays/BestTimeToBUYandsell.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        
+        mini = prices[0]
+        profit = 0
+        for i in range(len(prices)):
+            cost = prices[i] - mini
+            profit = max(cost,profit)
+
+            mini = min(mini,prices[i])
+
+        return profit
