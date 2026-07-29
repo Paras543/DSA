da9afe8295f19a401d187a866461ed42a2539b30
Paras543 <parasgupta0561@gmail.com>
2026-07-30 00:23:44 +0530

    Done

---
+++ Arrays/NextPermuation.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        
+        piv = -1
+        n = len(nums)
+        for i in range(n-2,-1,-1):
+            if nums[i] < nums[i+1]:
+                piv = i
+                break
+
+        if piv == -1:
+            nums.reverse()
+            return 
+
+        for i in range(n-1,-1,-1):
+            if nums[i] > nums[piv]:
+                nums[i] , nums[piv] = nums[piv] , nums[i]
+                break
+
+
+        i = piv+1
+        j = n-1
+        while(i<=j):
+
+            nums[i] , nums[j] = nums[j] , nums[i]
+            i += 1
+            j -= 1
+
+
+        
