7ee115604c6df3151ead7d6140f3afff92cd883e
Paras543 <parasgupta0561@gmail.com>
2026-07-31 15:09:38 +0530

    Done

---
+++ Arrays/DutchNationalFlag.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+
+
+        mid = 0
+        low = 0
+        high = len(nums) - 1
+
+        while(mid<=high):
+            if nums[mid] == 0:
+                nums[low],nums[mid] = nums[mid],nums[low]
+                low += 1
+                mid += 1
+
+            elif nums[mid] == 1:
+                mid += 1
+
+            else:
+                nums[high],nums[mid] = nums[mid] , nums[high]
+                high -= 1
+
+
+
+        
