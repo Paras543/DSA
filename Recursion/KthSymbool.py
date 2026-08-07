de58d8acb6656dd654a5305bc183f4f3f5134370
Paras543 <parasgupta0561@gmail.com>
2026-08-07 16:49:42 +0530

    Done

--- Recursion/KthSymbool.py
+++ Recursion/KthSymbool.py
@@ -15,3 +15,7 @@ class Solution(object):
             return self.kthGrammar(n-1,k)
         else:
             return 1 -  self.kthGrammar(n-1,k-mid)
+
+
+
+## revise it once more and the mid condtion is the mid = 2 ** n-2 where we calcualte the middle part and then traverse through the mid either we go left half or the right half
