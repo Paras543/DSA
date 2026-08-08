e019d577574e3da6480926417ddd34c5032ae5b1
Paras543 <parasgupta0561@gmail.com>
2026-08-08 13:44:31 +0530

    Done

--- Backtracking/Subsets II.py
+++ Backtracking/Subsets II.py
@@ -1,30 +1,32 @@
 class Solution(object):
     def allsubstes(self,nums,ans,i,all_subsets):
         if i == len(nums):
-            all_subsets.append(ans[:])
-            return 
-
-             
-
+             all_subsets.append(ans[:])
+             return 
         ans.append(nums[i])
         self.allsubstes(nums,ans,i+1,all_subsets)
         ans.pop()
-        self.allsubstes(nums,ans,i+1,all_subsets)
+
+        idx = i + 1
+        while idx < len(nums) and nums[idx] == nums[idx-1]:
+            idx += 1
+
+        self.allsubstes(nums,ans,idx,all_subsets)
 
 
-    def subsets(self, nums):
+
+
+    def subsetsWithDup(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
         """
+        nums.sort()
         all_subsets = []
         ans = []
+
         self.allsubstes(nums,ans,0,all_subsets)
 
         return all_subsets
-
-       
-
-        
-
-        
+    
+    
