d79a06078cbc9df75ba215155d929bb5d6adcdb6
Paras543 <parasgupta0561@gmail.com>
2026-08-08 14:15:09 +0530

    done

--- Backtracking/Permuations II .py
+++ Backtracking/Permuations II .py
@@ -3,23 +3,31 @@ class Solution(object):
         if idx == len(nums):
             ans.append(nums[:])
             return
-      
-        
-        
+        used = set()
 
         for i in range(idx,len(nums)):
+            if nums[i] in used:
+                continue
+            used.add(nums[i])
+            
             nums[idx] , nums[i] = nums[i] , nums[idx]
             self.getpermuations(nums,idx+1,ans)
+
             nums[idx] , nums[i] = nums[i] , nums[idx]
 
+       
+           
+                
 
-    def permute(self, nums):
+                
+
+    def permuteUnique(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        ans = []
-
+        ans= []
         self.getpermuations(nums,0,ans)
         return ans 
-        
+    
+    
